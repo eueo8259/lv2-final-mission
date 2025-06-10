dd97ed870640070ccafd49533207a3b589f32c4c
LJM\eueo8 <eueo8259@gmail.com>
2025-06-10 14:00:36 +0900

    feat: 고객 회원가입 API 구현

---
+++ src/main/java/finalmission/entity/Customer.java
@@ -0,0 +1,31 @@
+package finalmission.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(unique = true, nullable = false)
+    String email;
+
+    @Column(nullable = false)
+    String name;
+
+    public Customer (String email, String name){
+        this.email = email;
+        this.name = name;
+    }
+}
---
+++ src/main/java/finalmission/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package finalmission.repository;
+
+import finalmission.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
